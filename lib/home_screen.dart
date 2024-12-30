e67d09e0aec50fb419c181cac77c5c426ba3e9f5
Khaled <mechanic1080p@gmail.com>
2024-12-30 02:02:48 +0200

    Ui Enhancement

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,21 +10,20 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Electronics Stores",
             style: TextStyle(
-              fontFamily: 'Playfair Display',
-              fontSize: 30, 
-              fontWeight: FontWeight.bold,
-              color: const Color.fromARGB(255, 16, 53, 83))
-              ),
+                fontFamily: 'Playfair Display',
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(255, 16, 53, 83))),
         centerTitle: true,
         backgroundColor: Colors.blue.shade200,
       ),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      body: ListView(children: [
         Text(
           'Our Products',
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        Expanded(
-          flex: 1,
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.12,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemCount: productsList.length,
@@ -33,8 +32,8 @@ class MyHomePage extends StatelessWidget {
             },
           ),
         ),
-        Expanded(
-          flex: 3,
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.68,
           child: GridView.builder(
             gridDelegate:
                 SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
@@ -47,32 +46,34 @@ class MyHomePage extends StatelessWidget {
                   margin: const EdgeInsets.symmetric(horizontal: 6),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15),
-                    color: Colors.white,
+                    color: Theme.of(context).scaffoldBackgroundColor,
                     boxShadow: [
                       BoxShadow(
                         color: Colors.black.withValues(alpha: 0.5),
                         spreadRadius: 1,
-                        blurRadius: 9,
+                        blurRadius: 4,
                         offset: Offset(0, 1), // changes position of shadow
                       )
                     ],
                   ),
-                  child: Column(
-                    spacing: 3,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "${productsList[index][1]}",
-                        width: MediaQuery.of(context).size.width / 2,
-                        height: 100,
-                      ),
-                      Text(
-                        '${productsList[index][0]}',
-                        style: TextStyle(
-                            fontSize: 17, fontWeight: FontWeight.bold),
-                      ),
-                      Icon(Icons.add_shopping_cart)
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Column(
+                      spacing: 3,
+                      children: [
+                        Expanded(
+                          child: Image.asset(
+                            "${productsList[index][1]}",
+                          ),
+                        ),
+                        Text(
+                          '${productsList[index][0]}',
+                          style: TextStyle(
+                              fontSize: 17, fontWeight: FontWeight.bold),
+                        ),
+                        Icon(Icons.add_shopping_cart)
+                      ],
+                    ),
                   ),
                 ),
               );
@@ -83,36 +84,32 @@ class MyHomePage extends StatelessWidget {
           'Hot Offers',
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        Expanded(
-          flex: 1,
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.23,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemCount: productsList.length,
             itemBuilder: (context, index) {
               return Padding(
-                padding: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8.0),
                 child: Container(
-                  width: 120,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15),
-                    color: Colors.white,
+                    color: Theme.of(context).scaffoldBackgroundColor,
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.black.withValues(alpha: 0.5),
                         spreadRadius: 1,
-                        blurRadius: 9,
-                        offset: Offset(0, 1), // changes position of shadow
+                        blurRadius: 8,
+                        offset: Offset(0, 1),
+                        color: Colors.black.withValues(alpha: 0.5),
                       )
                     ],
                   ),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Expanded(
                         child: Image.asset(
-                          "${productsList[index][1]}",
-                          width: MediaQuery.of(context).size.width / 2,
-                          height: MediaQuery.of(context).size.height / 10,
+                          productsList[index][1],
                         ),
                       ),
                       Text(
